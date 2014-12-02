3d70980cae0db1963be36a0186677e92251fb141
Cyntwikip <jude_teves@dlsu.edu.ph>
2014-12-02 16:57:04 +0800

    First Upload... -Jude

--- Practice/teves_Lab1Ex3.m
+++
@@ -1,9 +0,0 @@
-%? ax
-
-function output = teves_Lab1Ex3(v, w)
-
-    output = sum(v.*w); % sum(var) --> sum of all elements in a vector 
-                    % v.*w --> product of elements of the same index
-
-
-end
